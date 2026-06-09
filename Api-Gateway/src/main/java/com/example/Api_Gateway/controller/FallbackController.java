34fb0fb27678a227ac5612e2dc4345dba1c383cc
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2026-06-09 18:34:50 +0530

    added circuit breaker

--- Api-Gateway/src/main/java/com/example/Api_Gateway/controller/FallbackController.java
+++ Api-Gateway/src/main/java/com/example/Api_Gateway/controller/FallbackController.java
@@ -4,28 +4,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/fallback")
 public class FallbackController {
 
-    @GetMapping("/fallback/auth")
+    @RequestMapping("/auth")
     public ResponseEntity<Map<String, Object>> authFallback() {
         return buildResponse("Auth Service is currently unavailable");
     }
 
-    @GetMapping("/fallback/action")
+    @RequestMapping("/action")
     public ResponseEntity<Map<String, Object>> actionFallback() {
         return buildResponse("Action Service is currently unavailable");
     }
 
-    @GetMapping("/fallback/query")
+    @RequestMapping("/query")
     public ResponseEntity<Map<String, Object>> queryFallback() {
         return buildResponse("Query Service is currently unavailable");
     }
 
-    @GetMapping("/fallback/ledger")
+    @RequestMapping("/ledger")
     public ResponseEntity<Map<String, Object>> ledgerFallback() {
         return buildResponse("Ledger Service is currently unavailable");
     }
@@ -34,9 +36,10 @@ public class FallbackController {
 
         Map<String, Object> response = new HashMap<>();
 
+        response.put("timestamp", LocalDateTime.now());
         response.put("status", "FAILURE");
-        response.put("message", message);
         response.put("code", 503);
+        response.put("message", message);
 
         return ResponseEntity
                 .status(HttpStatus.SERVICE_UNAVAILABLE)
